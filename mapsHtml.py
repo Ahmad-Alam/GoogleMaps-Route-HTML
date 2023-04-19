849178fd8670207c6acd7b371e1b1ce5c52d8e28
Ahmad Alam <ahmad22july@gmail.com>
2023-04-19 18:42:19 +0500

    Uploading the files.
    This is a simple script, you could just take input directly instead of the starting and ending locations being hardcoded. Maybe I'll change that in the next commit.

---
+++ mapsHtml.py
@@ -0,0 +1,32 @@
+import gmaps
+import googlemaps
+
+apiKey = 'AIzaSyAGezmSgwpnLcivR3Ja-OyMo2PyJuLvT5c'
+
+# Enter your Google Maps API key
+gmaps.configure(api_key=apiKey)
+google_maps = googlemaps.Client(key=apiKey)
+
+# Enter the starting and ending locations for the route
+start_loc = 'Rahim Yar Khan'
+end_loc = 'Lahore'
+
+# Get the directions for the route
+directions = google_maps.directions(start_loc, end_loc, mode="driving", units="metric")
+
+route_steps = [step['html_instructions'] for step in directions[0]['legs'][0]['steps']]
+
+
+# Open a new HTML file
+with open('route.html', 'w') as f:
+    # Write the HTML headers
+    f.write('<html><body><ol>')
+
+    # Loop through each step and write the html_instructions to the file
+    for step in route_steps:
+        f.write('<li>' + step + '</li>')
+
+    # Write the HTML footers
+    f.write('</ol></body></html>')
+
+print("All of the instructions have been printed onto the route.html file.")
